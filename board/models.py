87e76e3e69dcf57e09c14e4cfb69f3b2ad957aa9
Jessjesslee <asaprocky123@likelion.org>
2019-05-16 23:34:10 +0900

    initial commit

---
+++ board/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    update_date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(blank=True, upload_to='storedimages/')
+
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:50]
+
+
